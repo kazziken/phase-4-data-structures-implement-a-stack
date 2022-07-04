bf3eac2b71b42d09e068d969ec1adbc74ff7247e
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-07-04 17:01:35 -0400

    commit tree fe4c2388d2cb78c790dca9cdc82ff3a4bf0779d4

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,24 @@
 # your code here
+
+class Stack
+    attr_reader :stack
+
+    def initialize
+        @stack = []
+    end
+
+
+    def push(value)
+        stack.push(value)
+    end
+
+    def pop
+        stack.pop
+    end
+    
+    def peek
+        stack.last
+    end
+
+
+end
